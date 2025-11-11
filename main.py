8f11705b29f2992058f6bc7fb305d2c34deca6a7
2403a51068-lab <2403a51068@sru.edu.in>
2025-11-11 10:01:32 +0530

    Added multiply feature

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def add(a, b):
     return a + b
+def multiply(a, b):
+    return a * b
+
 
 def main():
     print("Addition:", add(5, 3))
+    print("Multiplication:", multiply(5, 3))
 
 if __name__ == "__main__":
     main()
